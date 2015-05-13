cc36bbe4ddfecc888a31a8e62af7cb3a1cd4429b
Matt Alhonte <mattalhonte@gmail.com>
2015-05-13 14:32:26 -0400

    Finish Exercise

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,80 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card
+        spc-vals {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (contains? spc-vals fst)
+      (spc-vals fst)
+      (Integer/valueOf (str fst)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    snd))
 
 (defn pair? [hand]
-  nil)
+  (>
+   (count
+    (filter
+     (fn [x] (== x 2))
+     (vals (frequencies (map rank hand)))))
+   0))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>
+   (count
+    (filter
+     (fn [x] (== x 3))
+     (vals (frequencies (map rank hand)))))
+   0))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>
+   (count
+    (filter
+     (fn [x] (== x 4))
+     (vals (frequencies (map rank hand)))))
+   0))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or
+     (>
+   (count
+    (filter
+     (fn [x] (== x 2))
+     (vals (frequencies (map rank hand)))))
+   1)
+     (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+ (let  
+          [sorted-rank  (sort (map rank hand))
+           lowest-card (first (sort (map rank hand)))
+           straight-final (+ lowest-card 5)
+           my-straight    (range lowest-card straight-final)
+           altern-ace (sort (replace {14 1} sorted-rank))]
+   (or
+        (= my-straight sorted-rank)
+        (= altern-ace (range 1 6)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+   (straight? hand)
+   (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
